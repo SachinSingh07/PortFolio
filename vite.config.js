f1e0d5f5b08f240ab84f68a2fa48ec013cf22f79
SachinSingh07 <sachinsingh07091999@gmail.com>
2024-07-23 22:24:56 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/my_portfolio/",
+ 
   plugins: [react()],
+  base:"/portfolio/",
 })
